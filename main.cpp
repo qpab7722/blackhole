fa432a88d0fd9cadd4c2ff18cdfd418719921328
qpab7722 <qpab7722@gmail.com>
2017-11-18 23:03:45 +0900

    스위치 누르면 가로로 전환

--- main.cpp
+++ main.cpp
@@ -20,9 +20,11 @@ COORD MT_pos = { 0,0 };
 int curPosX;
 int curPosY;
 int speed = 30;
+int check = 0; // 스위치후 delete
 
 int PCLife = 30;	//PC의 체력
 bool attacked = false;	//공격받았는지 알려주는 함수
+bool Switch_N = false; //스위치 충돌 알려주는 함수
 
 void RemoveCursor(void)
 {
@@ -100,18 +102,73 @@ void DeleteMT(char MeteoInfo[4][4])
 	SetCurrentCursorPos(curPos.X, curPos.Y);
 }
 
-void DrawOb(char GBInfo_N[29][25]) {
+void DeleteOb(char GBInfo_N[29][25])
+{
 	int x, y;
-
+	COORD curPos = GetCurrentCursorPos();
 	for (y = 0; y<29; y++)
 	{
 		for (x = 0; x<25; x++)
 		{
-			SetCurrentCursorPos((x * 2), y);
-			if (GBInfo_N[y][x] == 1)
-				printf("■");
-			if (GBInfo_N[y][x] == 2)
-				printf("▲");
+			SetCurrentCursorPos( (x * 2), y);
+			if (GBInfo_N[y][x]!=0)
+				printf(" ");
+		}
+	}
+	SetCurrentCursorPos(curPos.X, curPos.Y);
+}
+//PC를 지우는 함수
+void deletePC(char PCInfo[4][4])
+{
+	int x, y;
+	COORD curPos = GetCurrentCursorPos();
+	for (y = 0; y<4; y++)
+	{
+		for (x = 0; x<4; x++)
+		{
+			SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
+
+			if (PCInfo[y][x] == 1)
+				printf(" ");
+		}
+	}
+	SetCurrentCursorPos(curPos.X, curPos.Y);
+}
+
+void DrawOb(char GBinfo_N[29][25]) {
+	int x, y;
+
+	if (Switch_N)
+	{
+		if (check == 0)
+		{
+			DeleteOb(GBInfo_N[0]);
+			//deletePC(PCInfo[0]);
+
+		}
+		check++;
+	}
+	
+	for (y = 0; y<29; y++)
+	{
+		for (x = 0; x<25; x++)
+		{
+			if (Switch_N)
+			{				
+				SetCurrentCursorPos((y*2),x);		
+				
+			}
+			else
+			
+				SetCurrentCursorPos((x * 2), y);
+
+				if (GBinfo_N[y][x] == 1)
+					printf("■");
+				if (GBinfo_N[y][x] == 2)
+					printf("▲");
+				if (GBinfo_N[y][x] == 3)
+					printf("★");
+			
 		}
 	}
 }
@@ -123,17 +180,18 @@ int DrawMeteo()
 	DeleteMT(MeteoInfo[0]);
 
 	if (curPos.Y == 1) {
-	
+
 		return 0;
 	}
-	if (DetectCollision(curPos.X,  curPos.Y  + 1, MeteoInfo[0], GBInfo_N[0]) == 0)
+	if (DetectCollision(curPos.X, curPos.Y + 1, MeteoInfo[0], GBInfo_N[0]) == 0)
 	{
+		//DeleteOb(GBInfo_N[0]);
 		DrawOb(GBInfo_N[0]);
-	}  
+	}
 
 	MT_pos.Y -= 1;
 	SetCurrentCursorPos(MT_pos.X, MT_pos.Y);
-	ShowMT(MeteoInfo[0]);  
+	ShowMT(MeteoInfo[0]);
 	Sleep(50);
 	return 1;
 }
@@ -179,29 +237,18 @@ void drawPC(char PCInfo[4][4])
 	SetCurrentCursorPos(curPos.X, curPos.Y);
 }
 
-//PC를 지우는 함수
-void deletePC(char PCInfo[4][4])
-{
-	int x, y;
-	COORD curPos = GetCurrentCursorPos();
-	for (y = 0; y<4; y++)
-	{
-		for (x = 0; x<4; x++)
-		{
-			SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
 
-			if (PCInfo[y][x] == 1)
-				printf(" ");
-		}
-	}
-	SetCurrentCursorPos(curPos.X, curPos.Y);
-}
 
-int isCrash(int posX, int posY, char PCInfo[4][4], char GBInfo_N[29][25],char MeteoInfo[4][4])	//충돌 함수
+int isCrash(int posX, int posY, char PCInfo[4][4], char GBInfo_N[29][25], char MeteoInfo[4][4])	//충돌 함수
 {
 	int x, y;
 	int arrX = (posX) / 2;
 	int arrY = posY;
+	if (Switch_N)
+	{
+		arrX = posY+1;
+		arrY = posX/2-1;
+	}
 	for (x = 0; x<4; x++)
 	{
 		for (y = 0; y<4; y++)
@@ -217,12 +264,21 @@ int isCrash(int posX, int posY, char PCInfo[4][4], char GBInfo_N[29][25],char Me
 					attacked = true;
 					return 0;
 				}
+				if (!Switch_N && GBInfo_N[arrY + y][arrX + x] == 3)
+				{
+					Switch_N = true;
+					deletePC(PCInfo);
+					PC_pos.Y = 13;
+					MT_pos.X = 28;
+					MT_pos.Y = 3;
+					return 0;
+				}
 				/*
 				//메테오 위치 넣기
 				if (MeteoInfo[arrY + y +1][arrX + (x )] !=0 )	//운석이랑 부딪혔을때
 				{
-					attacked = true;
-				   
+				attacked = true;
+
 				}
 				*/
 			}
@@ -233,9 +289,10 @@ int isCrash(int posX, int posY, char PCInfo[4][4], char GBInfo_N[29][25],char Me
 
 int ShiftRight()
 {
-	if (isCrash(PC_pos.X + 2, PC_pos.Y, PCInfo[0], GBInfo_N[0],MeteoInfo[0]) == 0)
+	if (isCrash(PC_pos.X + 2, PC_pos.Y, PCInfo[0], GBInfo_N[0], MeteoInfo[0]) == 0)
 		return 0;
 	deletePC(PCInfo[0]);
+	//DeleteOb(GBInfo_N[0]);
 	DrawOb(GBInfo_N[0]);
 	PC_pos.X += 2;
 	SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
@@ -246,9 +303,10 @@ int ShiftRight()
 }
 int ShiftLeft()
 {
-	if (isCrash(PC_pos.X - 2, PC_pos.Y, PCInfo[0], GBInfo_N[0],MeteoInfo[0]) == 0)
+	if (isCrash(PC_pos.X - 2, PC_pos.Y, PCInfo[0], GBInfo_N[0], MeteoInfo[0]) == 0)
 		return 0;
 	deletePC(PCInfo[0]);
+	//DeleteOb(GBInfo_N[0]);
 	DrawOb(GBInfo_N[0]);
 	PC_pos.X -= 2;
 	SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
@@ -259,9 +317,10 @@ int ShiftLeft()
 
 int Jump()
 {
-	if (isCrash(PC_pos.X , PC_pos.Y - 1, PCInfo[0], GBInfo_N[0],MeteoInfo[0]) == 0||PC_pos.Y==0)
+	if (isCrash(PC_pos.X, PC_pos.Y - 1, PCInfo[0], GBInfo_N[0], MeteoInfo[0]) == 0 || PC_pos.Y == 0)
 		return 0;
 	deletePC(PCInfo[0]);
+	DeleteOb(GBInfo_N[0]);
 	DrawOb(GBInfo_N[0]);
 	PC_pos.Y -= 1;
 	SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
@@ -274,17 +333,19 @@ int Jump()
 int Gravity_N()
 {
 	//짧은 간격으로 한칸씩 내려서 두칸 내린다
-	if (isCrash(PC_pos.X, PC_pos.Y + 1, PCInfo[0], GBInfo_N[0],MeteoInfo[0]) == 0)
-		return 0;
+		if (isCrash(PC_pos.X, PC_pos.Y + 1, PCInfo[0], GBInfo_N[0], MeteoInfo[0]) == 0)
+			return 0;
 	deletePC(PCInfo[0]);
+	//DeleteOb(GBInfo_N[0]);
 	DrawOb(GBInfo_N[0]);
 	PC_pos.Y += 1;
 	SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
 	drawPC(PCInfo[0]);
 	Sleep(50);
+	
 	/*
 	if (isCrash(curPosX, curPosY + 1, PCInfo[0], GBInfo_N[0]) == 0)
-		return 0;
+	return 0;
 	deletePC(PCInfo[0]);
 	DrawOb(GBInfo_N[0]);
 	PC_pos.Y += 1;
@@ -326,11 +387,12 @@ int main(void)
 {
 
 	RemoveCursor();
+	//DeleteOb(GBInfo_N[0]);
 	DrawOb(GBInfo_N[0]);
 
 	MT_pos.Y = 25;
 	srand((unsigned int)time(NULL));
-	MT_pos.X = (rand() % 5)*2 + 10;
+	MT_pos.X = (rand() % 5) * 2 + 10;
 
 	while (1)
 	{
@@ -342,19 +404,15 @@ int main(void)
 		Gravity_N();
 		ProcessKeyInput();
 
-	//	SetCurrentCursorPos(60, 0 );
-	//	printf("                 ");	//윗줄에 나온거 지워주려고
 		SetCurrentCursorPos(60, 0);
 		printf("PC 체력: %3d", Physical(PCLife));
 		SetCurrentCursorPos(MT_pos.X, MT_pos.Y);
-		
+
 		if (DrawMeteo() == 0) {
 			getchar;
 			srand((unsigned int)time(NULL));
-			MT_pos.X = (rand() % 5)*2 + 10;
+			MT_pos.X = (rand() % 5) * 2 + 10;
 			MT_pos.Y = 25;
-			
-			
 		}
 	}
 
