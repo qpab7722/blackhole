4a4f45e377166510b26bd0a098bb688555aad539
gittttttgit <33508589+gittttttgit@users.noreply.github.com>
2017-11-18 23:38:34 +0900

    메테오까지 충돌 (전환에서 벽에 딱붙는거 못함)

--- main.cpp
+++ main.cpp
@@ -16,13 +16,15 @@
 */
 
 COORD PC_pos = { 10,0 };
+COORD MT_pos = { 0,0 };
 int curPosX;
 int curPosY;
 int speed = 30;
-int MT_posX;
-int MT_posY;
+int check = 0; // 스위치후 delete
+
 int PCLife = 30;	//PC의 체력
 bool attacked = false;	//공격받았는지 알려주는 함수
+bool Switch_N = false; //스위치 충돌 알려주는 함수
 
 void RemoveCursor(void)
 {
@@ -48,7 +50,7 @@ void SetCurrentCursorPos(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-int DetectCollision(int posX, int posY, char MeteoInfo[4][4], char ObInfo[25][25])
+int DetectCollision(int posX, int posY, char MeteoInfo[4][4], char GBInfo_N[25][25])
 {
 	int x, y;
 	int arrX = posX / 2;
@@ -58,7 +60,7 @@ int DetectCollision(int posX, int posY, char MeteoInfo[4][4], char ObInfo[25][25
 		for (y = 0; y < 4; y++) {
 			if (MeteoInfo[y][x] == 1 || MeteoInfo[y][x] == 2)
 			{
-				if (ObInfo[arrY + y + 1][arrX + (x * 2)] == 1 || ObInfo[arrY + y + 1][arrX + (x * 2)] == 2)
+				if (GBInfo_N[arrY + y + 1][arrX + (x * 2)] == 1 || GBInfo_N[arrY + y + 1][arrX + (x * 2)] == 2)
 					return 0;
 			}
 
@@ -100,18 +102,73 @@ void DeleteMT(char MeteoInfo[4][4])
 	SetCurrentCursorPos(curPos.X, curPos.Y);
 }
 
-void DrawOb(char ObInfo[25][25]) {
+void DeleteOb(char GBInfo_N[29][25])
+{
 	int x, y;
-
-	for (y = 0; y<25; y++)
+	COORD curPos = GetCurrentCursorPos();
+	for (y = 0; y<29; y++)
 	{
 		for (x = 0; x<25; x++)
 		{
 			SetCurrentCursorPos((x * 2), y);
-			if (ObInfo[y][x] == 1)
-				printf("□");
-			if (ObInfo[y][x] == 2)
-				printf("△");
+			if (GBInfo_N[y][x] != 0)
+				printf(" ");
+		}
+	}
+	SetCurrentCursorPos(curPos.X, curPos.Y);
+}
+//PC를 지우는 함수
+void deletePC(char PCInfo[4][4])
+{
+	int x, y;
+	COORD curPos = GetCurrentCursorPos();
+	for (y = 0; y<4; y++)
+	{
+		for (x = 0; x<4; x++)
+		{
+			SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
+
+			if (PCInfo[y][x] == 1)
+				printf(" ");
+		}
+	}
+	SetCurrentCursorPos(curPos.X, curPos.Y);
+}
+
+void DrawOb(char GBinfo_N[29][25]) {
+	int x, y;
+
+	if (Switch_N)
+	{
+		if (check == 0)
+		{
+			DeleteOb(GBInfo_N[0]);
+			//deletePC(PCInfo[0]);
+
+		}
+		check++;
+	}
+
+	for (y = 0; y<29; y++)
+	{
+		for (x = 0; x<25; x++)
+		{
+			if (Switch_N)
+			{
+				SetCurrentCursorPos((y * 2), x);
+
+			}
+			else
+
+				SetCurrentCursorPos((x * 2), y);
+
+			if (GBinfo_N[y][x] == 1)
+				printf("■");
+			if (GBinfo_N[y][x] == 2)
+				printf("▲");
+			if (GBinfo_N[y][x] == 3)
+				printf("★");
+
 		}
 	}
 }
@@ -123,18 +180,19 @@ int DrawMeteo()
 	DeleteMT(MeteoInfo[0]);
 
 	if (curPos.Y == 1) {
-		DrawOb(ObInfo[0]);
+
 		return 0;
 	}
-	if (DetectCollision(curPos.X, curPos.Y + 1, MeteoInfo[0], ObInfo[0]) == 0)
+	if (DetectCollision(curPos.X, curPos.Y + 1, MeteoInfo[0], GBInfo_N[0]) == 0)
 	{
-		DrawOb(ObInfo[0]);
+		//DeleteOb(GBInfo_N[0]);
+		DrawOb(GBInfo_N[0]);
 	}
 
-	MT_posY -= 1;
-	SetCurrentCursorPos(MT_posX, MT_posY);
+	MT_pos.Y -= 1;
+	SetCurrentCursorPos(MT_pos.X, MT_pos.Y);
 	ShowMT(MeteoInfo[0]);
-	Sleep(200);
+	Sleep(50);
 	return 1;
 }
 
@@ -159,7 +217,7 @@ int Physical(int maxLife)	//체력함수(캐릭터의 최대 체력을 받아서 현재 체력을 리�
 }
 
 //PC를 그리는 함수
-void drawPC(char PClnfo[4][4])
+void drawPC(char PCInfo[4][4])
 {
 	int x, y;
 	COORD curPos = GetCurrentCursorPos();
@@ -169,39 +227,28 @@ void drawPC(char PClnfo[4][4])
 		{
 			SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
 
-			if (PClnfo[y][x] == 1)
+			if (PCInfo[y][x] == 1)
 			{
 				if (y == 1) printf("◎");
-				if (y == 2)	printf("▲");
+				if (y == 2)	printf("△");
 			}
 		}
 	}
 	SetCurrentCursorPos(curPos.X, curPos.Y);
 }
 
-//PC를 지우는 함수
-void deletePC(char PClnfo[4][4])
-{
-	int x, y;
-	COORD curPos = GetCurrentCursorPos();
-	for (y = 0; y<4; y++)
-	{
-		for (x = 0; x<4; x++)
-		{
-			SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
 
-			if (PClnfo[y][x] == 1)
-				printf(" ");
-		}
-	}
-	SetCurrentCursorPos(curPos.X, curPos.Y);
-}
 
-int isCrash(int posX, int posY, char PCInfo[4][4], char ObInfo[25][25],char MeteoInfo[4][4])	//충돌 함수
+int isCrash(int posX, int posY, char PCInfo[4][4], char GBInfo_N[29][25], char MeteoInfo[4][4])	//충돌 함수
 {
 	int x, y;
 	int arrX = (posX) / 2;
 	int arrY = posY;
+	if (Switch_N)
+	{
+		arrX = posY + 1;
+		arrY = posX / 2 - 1;
+	}
 	for (x = 0; x<4; x++)
 	{
 		for (y = 0; y<4; y++)
@@ -209,19 +256,29 @@ int isCrash(int posX, int posY, char PCInfo[4][4], char ObInfo[25][25],char Mete
 			if (PCInfo[y][x] == 1)
 			{
 
-				if (ObInfo[arrY + y][arrX + x] == 1)	//벽이랑 부딪혔을때
+				if (GBInfo_N[arrY + y][arrX + x] == 1)	//벽이랑 부딪혔을때
 					return 0;
 
-				if (ObInfo[arrY + y][arrX + x] == 2)	//장애물이랑 부딪혔을때
+				if (GBInfo_N[arrY + y][arrX + x] == 2)	//장애물이랑 부딪혔을때
 				{
 					attacked = true;
 					return 0;
 				}
-
-				if (MeteoInfo[arrY + y -1][arrX + (x )] !=0 )	//운석이랑 부딪혔을때
+				if (!Switch_N && GBInfo_N[arrY + y][arrX + x] == 3)
+				{
+					Switch_N = true;
+					deletePC(PCInfo);
+					PC_pos.Y = 13;
+					MT_pos.X = 28;
+					MT_pos.Y = 3;
+					return 0;
+				}
+				
+				//메테오 위치 넣기
+				if ((PC_pos.X == MT_pos.X) && (PC_pos.Y + 4 == MT_pos.Y))	//운석 충돌 (서로 뚫고 지나감)
 				{
 					attacked = true;
-					printf("ee");
+					return 0;
 				}
 				
 			}
@@ -232,10 +289,11 @@ int isCrash(int posX, int posY, char PCInfo[4][4], char ObInfo[25][25],char Mete
 
 int ShiftRight()
 {
-	if (isCrash(curPosX + 2, curPosY, PCInfo[0], ObInfo[0],MeteoInfo[0]) == 0)
+	if (isCrash(PC_pos.X + 2, PC_pos.Y, PCInfo[0], GBInfo_N[0], MeteoInfo[0]) == 0)
 		return 0;
 	deletePC(PCInfo[0]);
-	DrawOb(ObInfo[0]);
+	//DeleteOb(GBInfo_N[0]);
+	DrawOb(GBInfo_N[0]);
 	PC_pos.X += 2;
 	SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
 	drawPC(PCInfo[0]);
@@ -245,10 +303,11 @@ int ShiftRight()
 }
 int ShiftLeft()
 {
-	if (isCrash(curPosX - 2, curPosY, PCInfo[0], ObInfo[0], MeteoInfo[0]) == 0)
+	if (isCrash(PC_pos.X - 2, PC_pos.Y, PCInfo[0], GBInfo_N[0], MeteoInfo[0]) == 0)
 		return 0;
 	deletePC(PCInfo[0]);
-	DrawOb(ObInfo[0]);
+	//DeleteOb(GBInfo_N[0]);
+	DrawOb(GBInfo_N[0]);
 	PC_pos.X -= 2;
 	SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
 	drawPC(PCInfo[0]);
@@ -258,10 +317,11 @@ int ShiftLeft()
 
 int Jump()
 {
-	if (isCrash(curPosX , curPosY - 1, PCInfo[0], ObInfo[0], MeteoInfo[0]) == 0)
+	if (isCrash(PC_pos.X, PC_pos.Y - 1, PCInfo[0], GBInfo_N[0], MeteoInfo[0]) == 0 || PC_pos.Y == 0)
 		return 0;
 	deletePC(PCInfo[0]);
-	DrawOb(ObInfo[0]);
+	DeleteOb(GBInfo_N[0]);
+	DrawOb(GBInfo_N[0]);
 	PC_pos.Y -= 1;
 	SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
 	drawPC(PCInfo[0]);
@@ -273,24 +333,26 @@ int Jump()
 int Gravity_N()
 {
 	//짧은 간격으로 한칸씩 내려서 두칸 내린다
-	if (isCrash(curPosX, curPosY + 1, PCInfo[0], ObInfo[0], MeteoInfo[0]) == 0)
+	if (isCrash(PC_pos.X, PC_pos.Y + 1, PCInfo[0], GBInfo_N[0], MeteoInfo[0]) == 0)
 		return 0;
 	deletePC(PCInfo[0]);
-	DrawOb(ObInfo[0]);
+	//DeleteOb(GBInfo_N[0]);
+	DrawOb(GBInfo_N[0]);
 	PC_pos.Y += 1;
 	SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
 	drawPC(PCInfo[0]);
 	Sleep(50);
 
-	//if (isCrash(curPosX, curPosY + 1, PCInfo[0], ObInfo[0], MeteoInfo[0]) == 0)
-	//	return 0;
-	//deletePC(PCInfo[0]);
-	//DrawOb(ObInfo[0]);
-	//PC_pos.Y += 1;
-	//SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
-	//drawPC(PCInfo[0]);
-	//Sleep(25);
-
+	/*
+	if (isCrash(curPosX, curPosY + 1, PCInfo[0], GBInfo_N[0]) == 0)
+	return 0;
+	deletePC(PCInfo[0]);
+	DrawOb(GBInfo_N[0]);
+	PC_pos.Y += 1;
+	SetCurrentCursorPos(PC_pos.X, PC_pos.Y);
+	drawPC(PCInfo[0]);
+	Sleep(25);
+	*/
 	return 1;
 }
 
@@ -298,7 +360,7 @@ void ProcessKeyInput()
 {
 	int key;
 
-	for (int i = 0;i<20;i++)
+	for (int i = 0; i<20; i++)
 	{
 		if (_kbhit() != 0)
 		{
@@ -325,11 +387,12 @@ int main(void)
 {
 
 	RemoveCursor();
-	DrawOb(ObInfo[0]);
+	//DeleteOb(GBInfo_N[0]);
+	DrawOb(GBInfo_N[0]);
 
-	MT_posY = 25;
+	MT_pos.Y = 25;
 	srand((unsigned int)time(NULL));
-	MT_posX = (rand() % 5)*2 + 10;
+	MT_pos.X = (rand() % 5) * 2 + 10;
 
 	while (1)
 	{
@@ -341,19 +404,15 @@ int main(void)
 		Gravity_N();
 		ProcessKeyInput();
 
-	//	SetCurrentCursorPos(60, 0 );
-	//	printf("                 ");	//윗줄에 나온거 지워주려고
 		SetCurrentCursorPos(60, 0);
 		printf("PC 체력: %3d", Physical(PCLife));
-		
-		SetCurrentCursorPos(MT_posX, MT_posY);
+		SetCurrentCursorPos(MT_pos.X, MT_pos.Y);
 
 		if (DrawMeteo() == 0) {
 			getchar;
 			srand((unsigned int)time(NULL));
-			MT_posX = (rand() % 5)*2 + 10;
-			SetCurrentCursorPos(MT_posX, 25);
-
+			MT_pos.X = (rand() % 5) * 2 + 10;
+			MT_pos.Y = 25;
 		}
 	}
 
